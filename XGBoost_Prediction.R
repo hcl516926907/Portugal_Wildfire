57241e7843638d5ec8316e014d8b91df62415297
chenglei_hu <chenglei_hu@163.com>
2025-01-22 00:51:38 +0000

    Removed unused code

--- XGBoost_Prediction.R
+++ XGBoost_Prediction.R
@@ -78,32 +78,6 @@ kfold_cv_pred  <- function(data, target.name ,covar.names, params, k) {
   return(data$score)
 }
 
-# load(file=file.path(dir.out,'XGBoost_z_council_hyperpara.RData'))
-# 
-# params_z <- list(
-#   nrounds = 100,
-#   eta = z_best_params['eta'],
-#   max_depth =  z_best_params['max_depth'],
-#   gamma = 0,
-#   scale_pos_weight = 2,
-#   colsample_bytree = z_best_params['colsample_bytree'],
-#   min_child_weight = z_best_params['min_child_weight'],
-#   subsample = z_best_params['subsample'],
-#   objective = "binary:logistic",
-#   eval_metric = "auc",
-#   verbosity = 0
-# )
-
-tune_grid_z <- expand.grid(
-  nrounds = 100,
-  eta = 0.07,
-  max_depth = 9,
-  gamma = 0,
-  scale_pos_weight = 700,
-  colsample_bytree = 0.7,
-  min_child_weight = 10,
-  subsample = 1
-)
 
 params_z <- list(
   nrounds = 100,
@@ -118,19 +92,19 @@ params_z <- list(
   verbosity = 0
 )
 
-#rolling window results
 # params_z <- list(
 #   nrounds = 100,
-#   eta = 0.05,
-#   max_depth = 7,
-#   gamma = 0.9,
+#   eta = 0.1,
+#   max_depth = 6,
+#   gamma = 1.3,
 #   scale_pos_weight = 6.87,
-#   colsample_bytree = 0.5,
-#   min_child_weight = 5,
+#   colsample_bytree = 0.6,
+#   min_child_weight = 16,
 #   subsample = 0.9,
 #   objective = "binary:logistic",
 #   verbosity = 0
 # )
+
 
 data.rf$score_z <- kfold_cv_pred(data.rf, 'z', covar.names ,params_z, 12)
 
@@ -145,165 +119,6 @@ data.fit[data.fit$year <= 2022, 'score_z'] <- data.rf$score_z
 data.fit[data.fit$year > 2022, 'score_z'] <- predict(model.z, as.matrix(data.fit[data.fit$year > 2022,c(covar.names)]))
 data.fit %>% group_by(year)%>% summarize(AUC=as.numeric(auc(z,score_z,quiet=TRUE)))
 
-#
-#
-# 
-# load(file=file.path(dir.out,'XGBoost_ba_council_hyperpara.RData'))
-# 
-# dextGP <- function(y,xi, sigma, kappa, log=FALSE){
-#   y1 <- 1+xi*y/sigma
-#   out.of.sup <- y1<=0 | y<=0
-#   y1[out.of.sup] <- NaN
-#   if (log){
-#     if (xi != 0){
-#       num <- log(kappa) + (kappa-1)*log(1-y1^(-1/xi))
-#       den <- log(sigma) + (1+1/xi)*log(y1)
-#     }else{
-#       num <- log(kappa) + (kappa-1)*log(1-exp(-y/sigma))
-#       den <- log(sigma) + y/sigma
-#     }
-#     res <- num-den
-#     res[which(out.of.sup)] <- -Inf
-#   }else{
-#     if(xi!=0){
-#       num <- kappa*(1-y1^(-1/xi))^(kappa-1)
-#       den <- sigma*y1^(1/xi +1)
-#     }else{
-#       num <- kappa*(1-exp(-y/sigma))^(kappa-1)
-#       den <- sigma*exp(y/sigma)
-#     }
-#     res <- num/den
-#     res[which(out.of.sup)] <- 0
-#   }
-#   return(res)
-# }
-# 
-# create_custom_objective <- function(xi, kappa, alpha) {
-#   function(preds, dtrain) {
-#     y <- getinfo(dtrain, "label")
-#     q_alpha <- exp(preds)
-#     
-#     # Compute sigma
-#     if (abs(xi) > 10^-6) {
-#       sigma <- xi * q_alpha / ( (1 - alpha^(1 / kappa))^(-xi) - 1 )
-#     } else {
-#       sigma <- -q_alpha / log(1 - alpha^(1 / kappa))
-#     }
-#     
-#     # Compute y1
-#     y1 <- 1 + xi * y / sigma
-#     
-#     # Handle invalid values
-#     invalid <- y1 <= 0 | y <= 0
-#     y1[invalid] <- NaN  # Avoid division by zero or log of non-positive numbers
-#     
-#     # Negative log-likelihood
-#     nll <- -dextGP(y, xi, sigma, kappa, log=TRUE)
-#     
-#     # Compute derivative of l w.r.t sigma
-#     if (abs(xi) > 10^-6) {
-#       # First derivative
-#       dl_dsigma <- (1 / sigma) - ((xi + 1) * y) / (sigma^2 * y1) + 
-#         ((kappa - 1) * y * y1^(-1 / xi - 1)) / (sigma^2 * (1 - y1^(-1 / xi)))
-#       
-#       # Second derivative (complex expression)
-#       # Compute components step by step
-#       term1 <- -1 / sigma^2
-#       
-#       term2 <- 2 * (xi + 1) * y / (sigma^3 * y1)
-#       term3 <- (xi + 1) * xi * y^2 / (sigma^4 * y1^2)
-#       
-#       term4_num <- 2 * y * y1^(-1 / xi - 1)
-#       term4_den <- sigma^3 * (1 - y1^(-1 / xi))
-#       term4 <- -(kappa - 1) * term4_num / term4_den
-#       
-#       term5_num <- (1 +  xi) * y^2 * y1^(-1 / xi - 2)
-#       term5_den <- sigma^4 * (1 - y1^(-1 / xi))
-#       term5 <- (kappa - 1) * term5_num / term5_den
-#       
-#       term6_num <-  y^2 * y1^(-2 / xi - 2)
-#       term6_den <- sigma^4 * (1 - y1^(-1 / xi))^2
-#       term6 <- (kappa - 1) * term6_num / term6_den
-#       
-#       d2l_dsigma2 <- term1 + term2 - term3 + term4 + term5 + term6
-#       
-#       # Compute gradient and hessian w.r.t pred
-#       
-#     } else {
-#       # For xi = 0, special case handling
-#       y2 <- exp(-y/sigma)
-#       dl_dsigma <- (1 / sigma) - y / sigma^2 + 
-#         ((kappa - 1) * y * y2) / (sigma^2 * (1 - y2))
-#       
-#       # Second derivative
-#       term1 <- -1 / sigma^2
-#       term2 <- 2 * y / sigma^3
-#       
-#       term3 <- (kappa - 1) * y2*y^2 / (sigma^4 * (1 - y2))
-#       
-#       term4_num <- 2*sigma*y2*(1-y2) - y2^2*y
-#       term4_den <- sigma^4*(1-y2)^2
-#       
-#       term4 <- (kappa-1)*y*term4_num/term4_den
-#       d2l_dsigma2 <- term1 + term2 + term3 - term4
-#       
-#       
-#     }
-#     
-#     grad <- sigma * dl_dsigma
-#     hess <- sigma^2 * d2l_dsigma2 + sigma * dl_dsigma
-#     
-#     # Handle NaNs or Infs
-#     grad[is.na(grad) | is.infinite(grad)] <- 0
-#     hess[is.na(hess) | is.infinite(hess)] <- 1  # To avoid division by zero in optimization
-#     
-#     return(list(grad = grad, hess = hess))
-#   }
-# }
-# 
-# loss_eval.egp <- function(preds, y, xi, kappa, alpha){
-#   q_alpha <- exp(preds)
-#   
-#   # Compute sigma based on xi
-#   sigma <- if (xi != 0) {
-#     xi * q_alpha / ( (1 - alpha^(1 / kappa))^(-xi) - 1 )
-#   } else {
-#     - q_alpha / log(1 - alpha^(1 / kappa))
-#   }
-#   
-#   return(-dextGP(y, xi, sigma, kappa, log=TRUE))
-# }
-
-
-
-# xi <- ba_best_params['xi']
-# kappa <- ba_best_params['kappa']
-# alpha <- ba_best_params['alpha']
-# 
-# custom_obj <- create_custom_objective(xi, kappa, alpha)
-
-
-# params_ba <- list(
-#   nrounds = 100,
-#   eta = ba_best_params['eta'],
-#   max_depth =  3,
-#   gamma = 0,
-#   colsample_bytree = 0.7,
-#   min_child_weight = 10,
-#   subsample = 0.9,
-#   objective=custom_obj
-# )
-# 
-# tune_grid <- expand.grid(
-#   nrounds = 100,
-#   eta = 0.08,
-#   max_depth = 4,
-#   gamma = 1,
-#   colsample_bytree = 0.8,
-#   min_child_weight = 7,
-#   subsample = 0.8,
-#   objective=c('reg:squarederror')
-# )
 
 params_ba <- list(
   nrounds = 95,
@@ -315,7 +130,18 @@ params_ba <- list(
   subsample = 0.6,
   objective='reg:squarederror'
 )
-# 
+
+# params_ba <- list(
+#   nrounds = 95,
+#   eta = 0.1,
+#   max_depth =  2,
+#   gamma = 9,
+#   colsample_bytree = 0.9,
+#   min_child_weight = 7,
+#   subsample = 0.6,
+#   objective='reg:squarederror'
+# )
+
 kfold_cv_pred_pos <- function(data, target.name ,covar.names, params, k) {
   loss_train_list <- numeric(k)
   loss_test_list <- numeric(k)
@@ -361,18 +187,6 @@ model.ba <- xgb.train(params = params_ba, data = TrainSet2, nrounds = params_ba$
 data.fit[data.fit$year <= 2022, 'score_ba'] <- data.rf$score_ba
 data.fit[data.fit$year > 2022, 'score_ba'] <- predict(model.ba, as.matrix(data.fit[data.fit$year > 2022,c(covar.names)]))
 
-# 
-# load(file=file.path(dir.out,'XGBoost_cnt_council_hyperpara.RData'))
-# params_cnt <- list(
-#   nrounds = 100,
-#   eta = cnt_best_params['eta'],
-#   max_depth = cnt_best_params['max_depth'],
-#   gamma = 0,
-#   colsample_bytree = cnt_best_params['colsample_bytree'],
-#   min_child_weight = cnt_best_params['min_child_weight'],
-#   subsample = cnt_best_params['subsample'],
-#   objective=c('count:poisson')
-# )
 
 
 trunc_poisson_loss <- function(y,preds){
@@ -414,6 +228,16 @@ params_cnt <- list(
   objective=trunc_poisson_obj
 )
 
+# params_cnt <- list(
+#   nrounds = 60,
+#   eta = 0.08,
+#   max_depth = 5,
+#   gamma = 0,
+#   colsample_bytree = 0.7,
+#   min_child_weight = 1,
+#   subsample = 1,
+#   objective=trunc_poisson_obj
+# )
 data.rf$score_cnt <- kfold_cv_pred_pos(data.rf,'y',covar.names, params_cnt, 12)
 
 
@@ -430,7 +254,7 @@ data.fit[data.fit$year > 2022 , 'score_cnt'] <- exp(predict(model.cnt, as.matrix
 # 
 data.fit[is.na(data.fit$log_ba),'log_ba'] <- 0
 
-data.fit %>% group_by(year)%>% summarize(AUC=as.numeric(auc(z,score_z,quiet=TRUE)))
+
 
 
 weighted_loss <- function(y, preds, type){
@@ -477,6 +301,8 @@ weighted_loss_z <- function(y, preds, cnt, area_ha){
   return(loss.cnt.1 + loss.ba.1 + loss.cnt.2 + loss.ba.2)
 }
 
+data.fit %>% group_by(year)%>% summarize(AUC=as.numeric(auc(z,score_z,quiet=TRUE)))
+
 data.fit[data.fit$y>0,]%>%
   group_by(year)%>% summarize(loss=weighted_loss(y,score_cnt,'CNT'))
 
@@ -486,4 +312,7 @@ data.fit[data.fit$y>0,]%>%
 
 
 save(data.fit, file=file.path(dir.out, 'XGBoost_Score_Council_2.RData'))
-save(covar.names,model.z, model.ba, model.cnt, file=file.path(dir.out, 'XGBoost_Models.RData'))
+
+
+save(data.rf, covar.names,model.z, model.ba, model.cnt, 
+     file=file.path(dir.out, 'XGBoost_Models.RData'))
